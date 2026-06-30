f1915076b2f27ab6125753ab66351425a697f3fe
Joel_Saldivia <saldiviajoel@gmail.com>
2026-06-29 23:36:54 -0300

    subo swagger

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 import express from "express";
-import cors from "cors";
-import morgan from "morgan";
-
+import swaggerUi from "swagger-ui-express";
+import { readFileSync } from "fs";
+import { parse } from "yaml";
 import especialidadesRoutes from "./src/routes/especialidades-routes.js";
 import obrasSocialesRoutes from "./src/routes/obras-sociales-routes.js";
 import medicosRoutes from "./src/routes/medicos-routes.js";
 import authRoutes from "./src/routes/auth-routes.js";
 import pacientesRoutes from "./src/routes/pacientes-routes.js";
 import turnosRoutes from "./src/routes/turnos-routes.js";
-import informesRoutes from "./src/routes/informes-routes.js";
+import estadisticasRoutes from "./src/routes/estadisticas-routes.js";
 
 const app = express();
 
-app.use(cors());
-app.use(morgan("dev"));
 app.use(express.json());
+
+// Swagger
+const swaggerDocument = parse(readFileSync("./swagger.yaml", "utf8"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/especialidades", especialidadesRoutes);
 app.use("/obras-sociales", obrasSocialesRoutes);
@@ -22,8 +24,9 @@ app.use("/medicos", medicosRoutes);
 app.use("/auth", authRoutes);
 app.use("/pacientes", pacientesRoutes);
 app.use("/turnos", turnosRoutes);
-app.use("/informes", informesRoutes);
+app.use("/estadisticas", estadisticasRoutes);
 
 app.listen(3000, () => {
   console.log("Servidor corriendo en puerto 3000");
+  console.log("Documentación disponible en http://localhost:3000/api-docs");
 });
