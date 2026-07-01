938bd5e8f409d5dee21ad486035906a5891366a4
Rosariogorosito <aldanagorosito802@gmail.com>
2026-07-01 18:11:13 -0300

    arreglo de conflictos

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import authRoutes from "./src/routes/auth-routes.js";
 import pacientesRoutes from "./src/routes/pacientes-routes.js";
 import turnosRoutes from "./src/routes/turnos-routes.js";
 import informesRoutes from "./src/routes/informes-routes.js";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
@@ -17,6 +19,8 @@ app.use(express.json());
 // Swagger
 const swaggerDocument = parse(readFileSync("./swagger.yaml", "utf8"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(cors());
+app.use(morgan("dev"));
 
 app.use("/especialidades", especialidadesRoutes);
 app.use("/obras-sociales", obrasSocialesRoutes);
